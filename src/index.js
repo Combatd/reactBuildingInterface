81a705a714337da35c52ec149a32de6f966951b4
Combatd <combatdstar@gmail.com>
2021-04-27 09:41:51 -0700

    Reorganize directories for Components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App';
+import './Styles/index.css';
+import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import 'jquery/dist/jquery';
 import 'popper.js/dist/umd/popper';
