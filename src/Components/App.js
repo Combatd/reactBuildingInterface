decf82b8849810b2834b249cb4627d530a288c9a
Combatd <combatdstar@gmail.com>
2021-05-01 16:28:58 -0700

    Define App#Constructor

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,6 +5,13 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myName: 'Mark',
+    }
+  }
+
   render() {
     return (
       <main className="page bg-white" id="petratings">
@@ -12,6 +19,7 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
+                { this.state.myName }
                 <AddAppointments />
                 <SearchAppointments />
                 <ListAppointments />
