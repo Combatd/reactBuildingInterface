83c329816e575a3ec015fe87240647e43580bd46
Combatd <combatdstar@gmail.com>
2021-05-24 16:48:51 -0700

    Define App#updateInfo for ListAppointments props

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,7 +5,7 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-import { without } from 'lodash';
+import { findIndex, without } from 'lodash';
 class App extends Component {
   constructor() {
     super();
@@ -55,6 +55,17 @@ class App extends Component {
       orderBy: order,
       orderDir: dir
     })
+  }
+
+  updateInfo = (name, value, id) => {
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
+    });
   }
 
   addAppointment(apt) {
@@ -125,7 +136,9 @@ class App extends Component {
                   searchApts={this.searchApts}
                 />
                 <ListAppointments appointments={filteredApts} 
-                  deleteAppointment={this.deleteAppointment} />
+                  deleteAppointment={this.deleteAppointment} 
+                  updateInfo={this.updateInfo}
+                  />
               </div>
             </div>
           </div>
