6306941cc33779c56a8a353abd6f3763c07a4dd8
Combatd <combatdstar@gmail.com>
2021-05-24 16:33:05 -0700

    Declare filteredAppointments as Higher Order Function in App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       formDisplay: false,
       orderBy: 'petName',
       orderDir: 'asc',
+      queryText: '',
       lastIndex: 0,
     }
     this.deleteAppointment = this.deleteAppointment.bind(this);
@@ -79,13 +80,25 @@ class App extends Component {
       order = -1;
     }
 
-    filteredApts.sort( (a, b) => {
+    filteredApts = filteredApts.sort( (a, b) => {
       if (a[this.state.orderBy].toLowerCase() < 
           b[this.state.orderBy].toLowerCase()) {
             return -1 * order;
           } else {
             return 1 * order;
           }
+    }).filter(eachItem => {
+      return(
+        eachItem['petName']
+        .toLowerCase()
+        .includes(this.state.queryText.toLowerCase()) ||
+        eachItem['ownerName']
+        .toLowerCase()
+        .includes(this.state.queryText.toLowerCase()) ||
+        eachItem['aptNotes']
+        .toLowerCase()
+        .includes(this.state.queryText.toLowerCase())
+      );
     });
 
     return (
